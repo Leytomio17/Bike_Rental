efa06dea2b2e2b4549898efee033d041d4fbc3a1
Leytomio17 <Leyton@atoba.ch>
2020-01-27 09:54:26 +0100

    first commit

---
+++ Bike_Rental/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Bike_Rental project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bike_Rental.settings')
+
+application = get_asgi_application()
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\vmadmin\PycharmProjects\Bike_Rental\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
