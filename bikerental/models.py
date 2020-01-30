efdccd1ef38bb5fc499409260bc93f795f2c6cad
Leyton Tomio <leyton@atoba.ch>
2020-01-30 11:31:27 +0100

    signup update

--- bikerental/models.py
+++ bikerental/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=300)
